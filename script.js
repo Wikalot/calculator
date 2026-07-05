dcda5cde423e16419d1b6d39645ff65dcac3f108
Wikalot <275708108+Wikalot@users.noreply.github.com>
2026-07-05 19:39:16 +0300

    Add operate function and variables for 2 numbers and an operator

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+
+let firstNum;
+let operator;
+let secondNum;
+
+function operate(num1, oper, num2) {
+    if (oper == "+") {
+        return add(num1, num2);
+    } else if (oper == "-") {
+        return subtract(num1, num2);
+    } else if (oper == "*") {
+        return multiply(num1, num2);
+    } else if (oper == "/") {
+        return divide(num1, num2);
+    }
+}
